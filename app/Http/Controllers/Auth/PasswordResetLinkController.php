57e84db9a76620db1e01b57a02fc94c182f00274
Milica <you@example.com>
2025-10-18 17:40:15 +0200

    Auth APIs revived

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
+use Illuminate\Http\JsonResponse; 
+
 
 class PasswordResetLinkController extends Controller
 {
---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Providers;
+
+class RouteServiceProvider
+{
+    // Where Breeze should redirect after web login/register
+    public const HOME = '/dashboard'; // or '/' if you don't have a dashboard
+}
